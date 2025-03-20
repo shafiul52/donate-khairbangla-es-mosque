8e5d67d4c51e6a8fe1cead7d7f1f4d350620ee0c
Your Name <shafiulhossain52@gmail.com>
2025-03-20 07:59:56 +0600

    ot loaction dot href equal dot slash home dot html added

---
+++ js/blog.js
@@ -0,0 +1,9 @@
+// add donate the amount 
+// step:1 add an event handler to the add donate button
+// step:2 get donation money to be added  account balance 
+document.getElementById('btn-blog')
+     .addEventListener('click',function(event) {
+        
+   window.location.href = './home.html'
+
+});
